5c3ea2d74c2b31183322ee088dbec5d0b4346e9f
asayvhwhy <asayvhwhy@gmail.com>
2026-03-10 10:27:07 +0530

    Application Entry & Welcome Message

---
+++ app1/src/PalindromeCheckerApp.java
@@ -0,0 +1,15 @@
+public class PalindromeCheckerApp {
+
+    public static void main(String[] args) {
+
+        System.out.println("      Welcome to the Application");
+
+        System.out.println("Application Name : Palindrome Checker");
+        System.out.println("Version          : 1.0");
+
+        System.out.println("System is starting...");
+        System.out.println("Ready to check palindromes.");
+
+        System.out.println("Proceeding to next operation...");
+    }
+}
